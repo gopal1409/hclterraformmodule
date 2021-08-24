ad41be141efa1da2736b8bb58cf3b31e31541aeb
Gopal <Gopal1409@gmail.com>
2021-08-24 02:59:26 -0700

    terrafomr module s3 bucket

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "arn" {
-  value = aws_s3_bucket.s3_bucket.arn 
+  value = modules.aws_s3_bucket.s3_bucket.arn 
 }
 output "name" {
-  value = aws_s3_bucket.s3_bucket.id 
+  value = modules.aws_s3_bucket.s3_bucket.id 
 }
 output "domain" {
-  value = aws_s3_bucket.s3_bucket.website_domain
+  value = modules.aws_s3_bucket.s3_bucket.website_domain
 }
 
 output "endpoint" {
-  value = aws_s3_bucket.s3_bucket.website_endpoint
+  value = modules.aws_s3_bucket.s3_bucket.website_endpoint
 }
