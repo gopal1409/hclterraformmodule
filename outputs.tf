8e930c3bdcd0fe14b515c76dd75e8d8cc4c949d6
Gopal <Gopal1409@gmail.com>
2021-08-24 03:33:17 -0700

    terrafomr module s3 bucket

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "arn" {
-  value = module.website_s3_bucket.arn 
-}
-output "name" {
-  value = module.website_s3_bucket.name 
-}
-output "domain" {
-  value = module.website_s3_bucket.domain
-}
-
 output "endpoint-aws" {
   value = module.website_s3_bucket.*
 }
