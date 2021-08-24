154bc2ad7f694da76994f7bfdda4f64d0809aa66
Gopal <Gopal1409@gmail.com>
2021-08-24 03:11:27 -0700

    terrafomr module s3 bucket

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "arn" {
-  value = modules.website_s3_bucket.arn 
+  value = module.website_s3_bucket.arn 
 }
 output "name" {
-  value = modules.website_s3_bucket.name 
+  value = module.website_s3_bucket.name 
 }
 output "domain" {
   value = modules.website_s3_bucket.domain
 }
 
 output "endpoint" {
-  value = modules.website_s3_bucket.endpoint
+  value = module.website_s3_bucket.endpoint
 }
