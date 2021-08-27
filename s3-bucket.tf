db7a94866db7f6b56fe7854780e22e17e9fe9851
Gopal <Gopal1409@gmail.com>
2021-08-27 01:55:00 -0700

    terrafomr s3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "website_s3_bucket" {
     source = "./modules/aws-s3-static-website-bucket"
     bucket_name = var.my_s3_bucket
-    acl = "private"
     key = "{var.folder_name}/"
     tags = var.my_s3_tag
 }
